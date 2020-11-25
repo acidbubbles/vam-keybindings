aadf21433dbec8614eb0668130cf76809d412761
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-11-24 23:41:14 -0500

    Add some basic useful bindings

--- src/BindableActions/BindableActions.cs
+++ src/BindableActions/BindableActions.cs
@@ -4,16 +4,20 @@
     {
         CreateTextField(new JSONStorableString("Description", "This plugin is used for bindings. It offers additional shortcuts not otherwise available using Virt-A-Mate triggers."));
 
-        RegisterString(new JSONStorableString("Print String", null, SuperController.LogMessage)
+        RegisterString(new JSONStorableString("Log Message", null, SuperController.LogMessage)
         {
             isStorable = false,
             isRestorable = false
         });
 
-        RegisterString(new JSONStorableString("Print Error String", null, SuperController.LogError)
+        RegisterAction(new JSONStorableAction("Clear Message Log", SuperController.singleton.ClearMessages));
+
+        RegisterString(new JSONStorableString("Log Error", null, SuperController.LogError)
         {
             isStorable = false,
             isRestorable = false
         });
+
+        RegisterAction(new JSONStorableAction("Clear Error Log", SuperController.singleton.ClearErrors));
     }
 }
