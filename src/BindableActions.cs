dd00fa04dfb357518d1fd785f01731d149e75167
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-11-24 23:01:03 -0500

    Proof of concept bindable actions storable

---
+++ src/BindableActions.cs
@@ -0,0 +1,19 @@
+﻿public class BindableActions : MVRScript
+{
+    public override void Init()
+    {
+        CreateTextField(new JSONStorableString("Description", "This plugin is used for bindings. It offers additional shortcuts not otherwise available using Virt-A-Mate triggers."));
+
+        RegisterString(new JSONStorableString("Print String", null, SuperController.LogMessage)
+        {
+            isStorable = false,
+            isRestorable = false
+        });
+
+        RegisterString(new JSONStorableString("Print Error String", null, SuperController.LogError)
+        {
+            isStorable = false,
+            isRestorable = false
+        });
+    }
+}
