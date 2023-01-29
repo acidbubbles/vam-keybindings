4e21ea29d536bc2f1d3a7ac86e697ccd572fd621
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-01-28 20:10:06 -0500

    ScenePluginTriggers plugin to map plugin actions as keybindings mappings

---
+++ src/ScenePluginTriggers/KeybindingsExtensions_ScenePluginTriggers.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MVR.FileManagementSecure;
+using SimpleJSON;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ReSharper disable once InconsistentNaming
+public class KeybindingsExtensions_ScenePluginTriggers : MVRScript
+{
+    private static readonly string _configPath = SuperController.singleton.savesDir + @"\PluginData\keybindingextensions_sceneplugintriggers.config";
+
+    private static readonly HashSet<string> _ignored = new HashSet<string>
+    {
+        "SaveToStore1",
+        "SaveToStore2",
+        "SaveToStore3",
+        "RestoreAllFromDefaults",
+        "RestorePhysicsFromDefaults",
+        "RestoreAppearanceFromDefaults",
+        "RestoreAllFromStore1",
+        "RestorePhysicsFromStore1",
+        "RestoreAppearanceFromStore1",
+        "RestoreAllFromStore2",
+        "RestorePhysicsFromStore2",
+        "RestoreAppearanceFromStore2",
+        "RestoreAllFromStore3",
+        "RestorePhysicsFromStore3",
+        "RestoreAppearanceFromStore3",
+    };
+
+    private readonly JSONStorableAction _refresh;
+    private readonly Dictionary<string, PluginTriggerActionBinding> _actions = new Dictionary<string, PluginTriggerActionBinding>();
+    private readonly Dictionary<string, PluginTriggerBoolBinding> _booleans = new Dictionary<string, PluginTriggerBoolBinding>();
+    private readonly JSONStorableString _enabledNamesJSON = new JSONStorableString("EnabledPluginTriggersList", "");
+    private readonly List<string> _enabledNames = new List<string>();
+    private readonly List<UIDynamic> _spacers = new List<UIDynamic>();
+
+    public KeybindingsExtensions_ScenePluginTriggers()
+    {
+        _refresh = new JSONStorableAction("RefreshList", UpdateList);
+    }
+
+    public override void Init()
+    {
+        if (containingAtom.type != "SessionPluginManager")
+        {
+            SuperController.LogError($"{nameof(KeybindingsExtensions_ScenePluginTriggers)} plugin can only be installed as a session plugin.");
+            CreateTextField(new JSONStorableString("Error", $"{nameof(KeybindingsExtensions_ScenePluginTriggers)} plugin can only be installed as a session plugin."));
+            enabledJSON.val = false;
+            return;
+        }
+
+        _enabledNamesJSON.setCallbackFunction = val =>
+        {
+            _enabledNames.Clear();
+            _enabledNames.AddRange(val.Split('\n').Where(x => x != ""));
+        };
+
+        if (FileManagerSecure.FileExists(_configPath))
+            RestoreFromJSON(LoadJSON(_configPath).AsObject);
+
+        Invoke(nameof(UpdateList), 0);
+    }
+
+    public override void InitUI()
+    {
+        base.InitUI();
+        if (UITransform == null) return;
+        leftUIContent.anchorMax = new Vector2(1, 1);
+    }
+
+    private void UpdateList()
+    {
+        foreach (var spacer in _spacers)
+            RemoveSpacer(spacer);
+        foreach (var action in _actions)
+            RemoveToggle(action.Value.enabledJSON);
+        _actions.Clear();
+        foreach (var boolean in _booleans)
+            RemoveToggle(boolean.Value.enabledJSON);
+        _booleans.Clear();
+
+        foreach (var atom in SuperController.singleton.GetAtoms())
+        {
+            foreach (var storable in atom.GetStorableIDs().Select(id => atom.GetStorableByID(id)))
+            {
+                if (!storable.name.StartsWith("plugin#")) continue;
+                var storableNameUnderscoreIndex = storable.name.IndexOf("_", StringComparison.Ordinal);
+                if (storableNameUnderscoreIndex == -1) continue;
+                var storableName = storable.name.Substring(storableNameUnderscoreIndex);
+
+                CreateTitle(storableName);
+
+                foreach (var n in storable.GetActionNames())
+                {
+                    if (_ignored.Contains(n)) continue;
+                    var ns = GetName(storableName, n);
+                    PluginTriggerActionBinding a;
+                    if (!_actions.TryGetValue(ns, out a))
+                    {
+                        a = new PluginTriggerActionBinding(ns)
+                        {
+                            enabledJSON = CreateEnabledToggle($"{n} (Action)", ns)
+                        };
+                        _actions.Add(ns, a);
+                    }
+                    a.Add(storable.GetAction(n));
+                }
+                foreach (var n in storable.GetBoolParamNames())
+                {
+                    var ns = GetName(storableName, n);
+                    PluginTriggerBoolBinding a;
+                    if (!_booleans.TryGetValue(ns, out a))
+                    {
+                        a = new PluginTriggerBoolBinding(ns)
+                        {
+                            enabledJSON = CreateEnabledToggle($"{n} (Toggle)", ns)
+                        };
+                        CreateToggle(a.enabledJSON);
+                        _booleans.Add(ns, a);
+                    }
+                    a.Add(storable.GetBoolJSONParam(n));
+                }
+            }
+        }
+
+        var missing = false;
+        foreach (var ns in _enabledNames)
+        {
+            if(_actions.ContainsKey(ns) || _booleans.ContainsKey(ns)) continue;
+            if (!missing)
+            {
+                CreateTitle("Not present in the scene");
+                missing = true;
+            }
+            CreateEnabledToggle(ns, ns);
+        }
+
+        transform.parent.parent.BroadcastMessage(nameof(IActionsInvoker.OnActionsProviderAvailable), this, SendMessageOptions.DontRequireReceiver);
+    }
+
+    private void CreateTitle(string storableName)
+    {
+        var title = CreateSpacer();
+        title.height = 46;
+        _spacers.Add(title);
+        var text = title.gameObject.AddComponent<Text>();
+        text.font = manager.configurableButtonPrefab.GetComponentInChildren<Text>().font;
+        text.alignment = TextAnchor.MiddleCenter;
+        text.raycastTarget = false;
+        text.fontSize = 28;
+        text.text = storableName;
+    }
+
+    private JSONStorableBool CreateEnabledToggle(string label, string ns)
+    {
+        var nameEnabledJSON = new JSONStorableBool(label, _enabledNames.Contains(ns), val => { ToggleName(val, ns); });
+        CreateToggle(nameEnabledJSON);
+        return nameEnabledJSON;
+    }
+
+    private void ToggleName(bool val, string ns)
+    {
+        if (val)
+            _enabledNames.Add(ns);
+        else
+            _enabledNames.Remove(ns);
+        _enabledNamesJSON.valNoCallback = string.Join("\n", _enabledNames.ToArray());
+        FileManagerSecure.CreateDirectory(@"Saves\PluginData");
+        SaveJSON(GetJSON(), _configPath);
+        UpdateList();
+    }
+
+    private static string GetName(string storable, string name)
+    {
+        return storable + "_" + name;
+    }
+
+    public void OnBindingsListRequested(ICollection<object> bindings)
+    {
+        bindings.Add(new Dictionary<string, string>
+        {
+            {"Namespace", "ScenePluginTriggers"}
+        });
+        bindings.Add(_refresh);
+        foreach (var binding in _actions)
+            if (_enabledNames.Contains(binding.Key))
+                bindings.Add(binding.Value.action);
+        foreach (var binding in _booleans)
+            if (_enabledNames.Contains(binding.Key))
+                bindings.Add(binding.Value.action);
+    }
+
+    public void OnDestroy()
+    {
+        transform.parent.parent.BroadcastMessage(nameof(IActionsInvoker.OnActionsProviderDestroyed), this, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public override JSONClass GetJSON(bool includePhysical = true, bool includeAppearance = true, bool forceStore = false)
+    {
+        var json = base.GetJSON(includePhysical, includeAppearance, forceStore);
+        _enabledNamesJSON.StoreJSON(json, includePhysical, includeAppearance, forceStore);
+        return json;
+    }
+
+    public override void RestoreFromJSON(JSONClass jc, bool restorePhysical = true, bool restoreAppearance = true, JSONArray presetAtoms = null, bool setMissingToDefault = true)
+    {
+        base.RestoreFromJSON(jc, restorePhysical, restoreAppearance, presetAtoms, setMissingToDefault);
+        _enabledNamesJSON.RestoreFromJSON(jc, restorePhysical, restoreAppearance, setMissingToDefault);
+    }
+}
---
+++ src/ScenePluginTriggers/PluginTriggerBinding.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public abstract class PluginTriggerBinding
+{
+    public JSONStorableBool enabledJSON { get; set; }
+    public abstract JSONStorableAction action { get; }
+}
+
+public class PluginTriggerActionBinding : PluginTriggerBinding
+{
+    public override JSONStorableAction action { get; }
+    private readonly List<JSONStorableAction> _targets = new List<JSONStorableAction>();
+
+    public PluginTriggerActionBinding(string name)
+    {
+        action = new JSONStorableAction(name, () =>
+        {
+            if (_targets.Count == 1)
+            {
+                _targets[0].actionCallback.Invoke();
+                return;
+            }
+
+            var selected = SuperController.singleton.GetSelectedAtom();
+            var target = _targets.FirstOrDefault(t => t.storable.containingAtom == selected);
+            if (target == null) return;
+            target.actionCallback.Invoke();
+        });
+    }
+
+    public void Add(JSONStorableAction value)
+    {
+        _targets.Add(value);
+    }
+}
+
+public class PluginTriggerBoolBinding : PluginTriggerBinding
+{
+    public override JSONStorableAction action { get; }
+    private readonly List<JSONStorableBool> _targets = new List<JSONStorableBool>();
+
+    public PluginTriggerBoolBinding(string name)
+    {
+        action = new JSONStorableAction(name, () =>
+        {
+            if (_targets.Count == 1)
+            {
+                _targets[0].val = !_targets[0].val;
+                return;
+            }
+
+            var selected = SuperController.singleton.GetSelectedAtom();
+            var target = _targets.FirstOrDefault(t => t.storable.containingAtom == selected);
+            if (target == null) return;
+            target.val = !target.val;
+        });
+    }
+
+    public void Add(JSONStorableBool param)
+    {
+        _targets.Add(param);
+    }
+}
+
